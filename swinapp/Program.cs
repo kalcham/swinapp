e2a9a9ef72d7c304d8428caf6e56f64e4c7a014a
Kallum Chamberlain-Priest <kalcham@chbc.school.nz>
2023-02-07 12:52:00 +1300

    Add project files.

---
+++ swinapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace swinapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
